6e6dddade45c4922d3967cdd526497f6ccc5c9e3
Henrique-Sant <henrique2003santiago@gmail.com>
2026-09-24 23:01:48 +0000

    primeiro commit

---
+++ trabalho/src/test/java/br/pucrs/henriqueeheitor/trabalho/TrabalhoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pucrs.henriqueeheitor.trabalho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
